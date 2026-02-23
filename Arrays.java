4bbd1bc4b31fcc85f020e295f640effe7a8f11f4
ShaikTouseef04 <shaiktouseef04@gmail.com>
2026-02-23 15:17:17 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+package mycodes;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers= {10,20,30,50,55,60,40};
+		for(int i=0;i< numbers.length;i++) {
+			System.out.println(numbers[i]);
+		}
+	}
+
+}
